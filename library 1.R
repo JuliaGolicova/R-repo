75549d5864cc184b4f670592682e368403a145a1
JuliaGolicova <golicova@gmail.com>
2020-10-03 21:45:58 +0300

    -

--- library 1.R
+++ library 1.R
@@ -4,4 +4,10 @@ library(RMySQL)
 cnx <- dbConnect(MySQL(), user="thinkorswim", password="tr&des1ation", 
                  host="db4free.net", dbname="thinkorswim")
 trade.blotter <- dbReadTable(cnx, "trade_blotter")
+trade.blotter$trade_time <- strptime(str_c(trade.blotter$trade_date, trade.blotter$trade_time, sep=" "), 
+                                     format="%m/%d/%Y %I:%M:%S %p")
+trade.blotter$trade_date <- as.Date(trade.blotter$trade_date, "%m/%d/%y")
+trade.blotter$settle_date <- as.Date(trade.blotter$settle_date, "%m/%d/%y")
+forts.cash <- dbReadTable(cnx, "forts_cash")
+
 dbDisconnect(cnx)
