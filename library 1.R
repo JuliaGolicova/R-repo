f7c131d0be0a2e8167cf1a7313587a4bca891400
JuliaGolicova <golicova@gmail.com>
2020-10-03 18:36:16 +0300

    -

--- library 1.R
+++ library 1.R
@@ -1,1 +1,7 @@
 library(tidyverse)
+library(RMySQL)
+
+cnx <- dbConnect(MySQL(), user="thinkorswim", password="tr&des1ation", 
+                 host="db4free.net", dbname="thinkorswim")
+trade.blotter <- dbReadTable(cnx, "trade_blotter")
+dbDisconnect(cnx)
